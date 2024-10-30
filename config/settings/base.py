f404aaa1b9047e68760e7b2b09a61403e1e12c46
mehran_safaripour <mehran.safaripour@gmail.com>
2024-10-30 20:32:58 +0330

    refactor: add orders app to LOCAL_APPS

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,6 +85,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "abantether.users",
+    "abantether.orders"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
